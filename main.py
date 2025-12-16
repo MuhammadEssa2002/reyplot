bb91fe9e1576755cdb56ab6ffed5b21b6fee0701
Muhammad Essa <essabahootraza@gmail.com>
2025-12-17 02:00:09 +0500

    Updated

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import reyplot.plot as rlt
+import reyplot.plot as rlt 
 
 df = rlt.load_dataset("iris")
-
 
 
 rlt.chart(size = [1000,800])
 
 rlt.scatter(data = df,
-            x = "petal_width",
-            y = "petal_length",
-            title = "petal_width vs petal_length",
+            x = "sepal_width",
+            y = "sepal_length",
             color_by = "petal_length"
-           )
-rlt.legend()
-rlt.title("Iris Data", font = "Bruno Ace")
+            )
+
+rlt.title("Iris Data")
 rlt.show()
